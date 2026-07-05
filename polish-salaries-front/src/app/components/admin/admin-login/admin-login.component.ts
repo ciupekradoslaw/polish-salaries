0735c5e83f43791ba653a152ef536d5346323b02
ciupekradoslaw <radoslawciupek@outlook.com>
2026-07-05 21:34:37 +0200

    Modifications in material-theme.scss

--- polish-salaries-front/src/app/components/admin/admin-login/admin-login.component.ts
+++ polish-salaries-front/src/app/components/admin/admin-login/admin-login.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { TranslatePipe } from '@ngx-translate/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { form, FormField, required, submit } from '@angular/forms/signals';
+import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
+import { MatButton } from '@angular/material/button';
+import { MatDivider } from '@angular/material/list';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-admin-login',
-  imports: [MatIcon, TranslatePipe],
+  imports: [
+    MatIcon,
+    TranslatePipe,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormField,
+    MatLabel,
+    MatInput,
+    FormField,
+    MatButton,
+    MatDivider,
+    RouterLink
+  ],
   templateUrl: './admin-login.component.html',
   styleUrl: './admin-login.component.css'
 })
-export class AdminLoginComponent {}
+export class AdminLoginComponent {
+  loginModel = signal({
+    login: '',
+    password: ''
+  });
+
+  loginForm = form(this.loginModel, (login) => {
+    required(login.login);
+    required(login.password);
+  });
+
+  async onSubmit() {
+    await submit(this.loginForm, async (form) => {
+      return undefined;
+    });
+  }
+}
